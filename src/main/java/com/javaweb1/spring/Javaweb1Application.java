fce02934ceaa287bb7a9be598d40a171379e5f16
HoangTiepLukas <hoang.vantiep@1pf.cz>
2026-05-21 13:14:53 +0200

    Initial commit

---
+++ src/main/java/com/javaweb1/spring/Javaweb1Application.java
@@ -0,0 +1,13 @@
+package com.javaweb1.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaweb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Javaweb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaweb1/spring/Javaweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaweb1.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaweb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
